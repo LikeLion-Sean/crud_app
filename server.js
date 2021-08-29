bed09aaf8053f9adedbb55aedc5fe228e6015fcd
Sean <sean.kim84@likelion.net>
2021-08-29 19:58:41 +0700

    port modified

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static('public'));
 app.use('/blogs', blogRouter);
 
 //listen port
-const port = "https://likelion-crudapp.herokuapp.com/" || 5000;
-app.listen(port, () => {
+const port = 5000;
+app.listen(process.env.PORT || port, () => {
   console.log(`Server is learning on http://localhost:${port}`)
 });
